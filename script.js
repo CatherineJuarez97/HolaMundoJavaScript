ba73827a604b2c3e081a939bdcffa99ed93407e9
Cath <92835425+CatherineJuarez97@users.noreply.github.com>
2023-07-30 23:11:22 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/*const nombre = "Juan Peréz"; 
+let edad = 22;
+let decision = false;
+
+if(edad < 18){
+    console.log(`${nombre} es mayor de edad`)
+} else{
+    console.log(`${nombre} es menor de edad`)
+
+}/*
+
